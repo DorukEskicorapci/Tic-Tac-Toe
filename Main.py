fba4df0f7cef1287dea7249a47c07c1efa0d4a3c
Doruk Eskicorapci <de295@rutgers.edu>
2024-08-27 11:23:19 +0300

    userinput feature created

--- Main.py
+++ Main.py
@@ -1,21 +1,33 @@
 from game import tablefunctions, Coordinates
 
-x, y = 3, 3
+#x, y = 3, 3
+
+size = input("Enter the board size: ")
+x, y = int(size),int(size)
 
 if x > 9 or y > 9:
-    print("Board size too large")
+    print("Board size too large. Please enter a value less than 10." + "\n")
     exit()
 
-gameboard = tablefunctions.createboard(x + 1, y + 1)
+gameboard = tablefunctions.createboard(x, y)
+
+tablefunctions.displayboard(gameboard)
+
+user1move = input("Enter the coordinates for player 1: ")
+
+move_x = Coordinates.translate_x(user1move)
+move_y = Coordinates.translate_y(user1move)
+
+gameboard[move_y][move_x] = "X"
 
 
 
-
-tablefunctions.updateboard(gameboard, 1, 1, "O")
+tablefunctions.updateboard(gameboard, 1, 1, "x")
 
 
 tablefunctions.displayboard(gameboard)
 
+"""
 tablefunctions.updateboard(gameboard, 2, 1, "X")
 
 tablefunctions.displayboard(gameboard)
@@ -24,7 +36,7 @@ tablefunctions.updateboard(gameboard, 3, 3, "X")
 
 tablefunctions.displayboard(gameboard)
 
-
+"""
 
 '''
 #command for printing the whole board 
--- game.py
+++ game.py
@@ -1,8 +1,20 @@
 class Coordinates:
-    def __init__():
+    def translate_x(usermove_x):
         
+        move_x = usermove_x[0]
+        lowercase = move_x.lower()
+        i = 0
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        while i < len(letters):
+            if lowercase == letters[i]:
+                return i +1
+                break
+            i += 1
+
+    
+    def translate_y(usermovey):
         
-        print("obj")
+        return int(usermovey[1])
 
 
 class tablefunctions:
@@ -10,15 +22,17 @@ class tablefunctions:
     def createboard(x, y):
         
         # ideal values: x, y = 3, 3
+        x = x + 1
+        y = y + 1
         i = 0
         gameboard = [['▢' for _ in range(x)] for _ in range(y)]
 
-       
 
         letters = " abcdefghijklmnopqrstuvwxyz"
         while i < y:
             gameboard[0][i] = letters[i]
             i += 1
+
         i = 0
         while i < x:
             gameboard[i][0] = i
@@ -26,11 +40,8 @@ class tablefunctions:
         gameboard[0][0] = " "
 
 
-            
-
         return gameboard
 
-    
     
     def displayboard(gameboard):
         i = 0
