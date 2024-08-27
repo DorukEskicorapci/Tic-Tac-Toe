a7f8a409e2ebc5f36bcf2fff2b67eea0006b2f0e
Doruk Eskicorapci <de295@rutgers.edu>
2024-08-27 16:47:25 +0300

    first playable version (prototype)

--- Main.py
+++ Main.py
@@ -2,6 +2,9 @@ from game import tablefunctions, Coordinates
 
 #x, y = 3, 3
 
+#creates the board
+check = False
+
 size = input("Enter the board size: ")
 x, y = int(size),int(size)
 
@@ -13,21 +16,47 @@ gameboard = tablefunctions.createboard(x, y)
 
 tablefunctions.displayboard(gameboard)
 
-user1move = input("Enter the coordinates for player 1: ")
-
-move_x = Coordinates.translate_x(user1move)
-move_y = Coordinates.translate_y(user1move)
-
-gameboard[move_y][move_x] = "X"
 
 
+#starts taking user input for game moves
+i = 1
+while check == False:
+   usermove = input("Enter the coordinates for player " + str(i) + ": ")
 
+   move_x = Coordinates.translate_x(usermove)
+   move_y = Coordinates.translate_y(usermove)
+   if i == 1:
+      i = 2
+      gameboard[move_y][move_x] = "X"
+        
+   else:
+      i = 1
+      gameboard[move_y][move_x] = "O"
+
+   
+   tablefunctions.displayboard(gameboard)
+
+   usermove = input("Do you want to end the game. Enter 'yes' or 'no': ")
+   if usermove == "yes":
+        check = True
+      
+        
+
+
+
+
+
+def checkwin(gameboard):
+    
+
+
+  """ update and display the board
 tablefunctions.updateboard(gameboard, 1, 1, "x")
 
 
 tablefunctions.displayboard(gameboard)
 
-"""
+
 tablefunctions.updateboard(gameboard, 2, 1, "X")
 
 tablefunctions.displayboard(gameboard)
@@ -36,21 +65,35 @@ tablefunctions.updateboard(gameboard, 3, 3, "X")
 
 tablefunctions.displayboard(gameboard)
 
-"""
 
-'''
 #command for printing the whole board 
 
 for xs in gameboard:
     print(*xs, sep="\t")
             
-'''
+"""
 
+""" scenarios  
+      y1 y2 y3         
+   x1 x  x  x
+   x2 -  -  -
+   x3 -  -  -
 
+      y1 y2 y3       
+   x1 x  -  -
+   x2 -  x  -
+   x3 -  -  x
+
+      y1 y2 y3       
+   x1 x  -  -
+   x2 x  -  -
+   x3 x  -  -
+
+"""
 
 #Coordinates.obj.print_value()
 
-'''  
+'''  coordinates for the board
       y1 y2 y3       
    x1 -  -  -
    x2 -  -  -
--- game.py
+++ game.py
@@ -18,6 +18,8 @@ class Coordinates:
         return int(usermovey[1])
 
 
+
+
 class tablefunctions:
 
     def createboard(x, y):
